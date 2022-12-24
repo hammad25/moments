afed2d6341ad1d54a95e83bc659857c82d4cc8e6
Hammad <hammad2091504@hotmail.com>
2022-12-24 10:37:42 +0000

    Add App and NavBar styles

---
+++ src/components/ES7Snippets.js
@@ -0,0 +1,53 @@
+// rfce
+// Create a React Function
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rafce
+// Arrow Function
+// import React from 'react'
+
+// export const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// rce
+// Create a React Class Export
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// rconst
+// Create a React Constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// cle
+// Console.log()
+// console.log(object)
+
+// clo
+// Console.log(name + object)
+// console.log('object', object)
